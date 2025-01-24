98d5015d24c97f4879d2a0eb6e826977a8e28f6b
GTA__Civilian <karabomunano@gmail.com>
2025-01-24 01:50:45 -0800

    Delete opp directory

--- opp/main.py
+++
@@ -1,8 +0,0 @@
-from opp import book_class
-def main():
-    book = book_class.Book("1984", "George Orwell", 1949)
-    print(book)
-    print(repr(book))
-    del book
-if __name__ == "__main__":
-    main()
