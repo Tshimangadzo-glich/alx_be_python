25e7f685b37accea86c48f083741df846b012765
GTA__Civilian <karabomunano@gmail.com>
2025-01-24 10:55:57 -0800

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,14 +2,12 @@ class Calculator:
     @classmethod 
     def add (a, b):
         return a + b
-    
 @classmethod
     def multiply(cls, a: float, b: float) -> float:
         print(f"Performing {cls.calculation_type}...")
         return a * b
-["Arithmetic Operations"]
-
+"Arithmetic Operations"
 @staticmethod
     def add(a: float, b: float) -> float:
         return a + b
-["multiply(cls, a, b)"]
+"multiply(cls, a, b)"
