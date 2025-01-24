aeb2de348aa65475c28642c716cdd5b6600067d3
GTA__Civilian <karabomunano@gmail.com>
2025-01-24 11:01:29 -0800

    Add files via upload

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+from oop.polymorphism_demo import rectangle
+from oop.polymorphism_demo import Circle
+class Shape:
+    def area(self):
+        class Rectangle(Shape):
+            def area(self):
+                def __init__(self, length: float, width: float):
+                    self.length = length
+                    self.width = width
+
+def area(self):
+    return self.length * self.width
+class Circle:
+    def area(self):
+        return math.p1 * (self.radius ** 2)
+    if __name__ == "__main__":
+                    self.rectangle = rectangle
+                    self.Circle = Circle
+                    rectangle = rectangle(5, 3)
+                    circle = Circle(4)
+                    print(f"Rectangle area: {rectangle.area()}")
+                    print(f"Circle area: {circle.area()}")
+                    
