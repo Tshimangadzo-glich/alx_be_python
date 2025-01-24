e3322093cfcfc63b69ccefec7723cd3728acbdc9
GTA__Civilian <karabomunano@gmail.com>
2025-01-24 11:08:00 -0800

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,3 @@
-from gc import get_count
-
 class Book:
    def __init__(self, title, author):
     title = title
@@ -23,21 +21,19 @@ class Librabry:
    def __init__(self):
     self.Book [""]
     self.page_count = get_count
-    
     print(f"Title: {Book.title}")
     print(f"Author: {Book.author}")
-
+      
 def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
-    
 def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
-    
+        return f"Book('{self.title}', '{self.author}', {self.year})"  
 def __del__(self):
         print("Deleting", self.title)
-
+   
 if isinstance(Book, Ebook):
                 print(f"File Size: {Book.file_size} MB")
 elif isinstance(Book, Printbook, ):
                 print(f"Page Count: {Book.get_count} pages")        
 print("------------------------")
+super().__init__", "self.file_size
